c3b50b14d720acc5fa00af32157fd9526f00c1a4
Moricchi <ptnadia337@gmail.com>
2024-04-26 15:06:54 +0800

    scroll

--- lib/anime_screen.dart
+++ lib/anime_screen.dart
@@ -18,12 +18,22 @@ class _AnimeScreenState extends State<AnimeScreen> {
   late int? animeIdForUpdate;
   late DBHelper dbHelper;
   final _animeTitleController = TextEditingController();
+  late String _status;
+  late int _progress;
+  late double _score;
+  late DateTime _startDate;
+  late DateTime _endDate;
 
   @override
   void initState() {
     super.initState();
     dbHelper = DBHelper();
     refreshAnimeLists();
+    _status = 'Watching';
+    _progress = 0;
+    _score = 0.0;
+    _startDate = DateTime.now();
+    _endDate = DateTime.now();
   }
 
   @override
@@ -61,9 +71,11 @@ class _AnimeScreenState extends State<AnimeScreen> {
   void createOrUpdateAnime() {
     _formStateKey.currentState?.save();
     if (!isUpdate) {
-      dbHelper.add(Anime(null, _title));
+      dbHelper.add(Anime(
+          null, _title, _status, _progress, _score, _startDate, _endDate));
     } else {
-      dbHelper.update(Anime(animeIdForUpdate, _title));
+      dbHelper.update(Anime(animeIdForUpdate, _title, _status, _progress,
+          _score, _startDate, _endDate));
     }
     _animeTitleController.text = '';
     refreshAnimeLists();
@@ -73,6 +85,12 @@ class _AnimeScreenState extends State<AnimeScreen> {
     setState(() {
       isUpdate = true;
       animeIdForUpdate = anime.id!;
+      _title = anime.title;
+      _status = anime.status;
+      _progress = anime.progress;
+      _score = anime.score ?? 0.0;
+      _startDate = anime.startDate ?? DateTime.now();
+      _endDate = anime.endDate ?? DateTime.now();
     });
     _animeTitleController.text = anime.title;
   }
@@ -107,63 +125,170 @@ class _AnimeScreenState extends State<AnimeScreen> {
               TextStyle(color: !isUpdate ? Colors.purple : Colors.blue)),
     );
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Form(
-            key: _formStateKey,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: textFormField,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Form(
+                key: _formStateKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        children: [
+                          textFormField,
+                          DropdownButtonFormField<String>(
+                            value: _status,
+                            onChanged: (value) {
+                              setState(() {
+                                _status = value!;
+                              });
+                            },
+                            items: ['Watching', 'Completed', 'Plan to Watch']
+                                .map<DropdownMenuItem<String>>((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            decoration: const InputDecoration(
+                              labelText: 'Status',
+                              icon: Icon(Icons.info),
+                            ),
+                          ),
+                          TextFormField(
+                            onSaved: (value) {
+                              _progress = int.parse(value!);
+                            },
+                            keyboardType: TextInputType.number,
+                            decoration: const InputDecoration(
+                              labelText: "Episode Progress",
+                              icon: Icon(Icons.play_arrow),
+                            ),
+                          ),
+                          TextFormField(
+                            onSaved: (value) {
+                              _score = double.parse(value!);
+                            },
+                            keyboardType: const TextInputType.numberWithOptions(
+                                decimal: true),
+                            decoration: const InputDecoration(
+                              labelText: 'Score',
+                              icon: Icon(Icons.star),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              final DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                initialDate: _startDate,
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2100),
+                              );
+                              if (pickedDate != null &&
+                                  pickedDate != _startDate) {
+                                setState(() {
+                                  _startDate = pickedDate;
+                                });
+                              }
+                            },
+                            child: AbsorbPointer(
+                              child: TextFormField(
+                                controller: TextEditingController(
+                                  text: _startDate.toString().split(' ')[0],
+                                ),
+                                decoration: const InputDecoration(
+                                  labelText: 'Start Date',
+                                  icon: Icon(Icons.calendar_today),
+                                ),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () async {
+                              final DateTime? pickedDate = await showDatePicker(
+                                context: context,
+                                initialDate: _endDate,
+                                firstDate: DateTime(2000),
+                                lastDate: DateTime(2100),
+                              );
+                              if (pickedDate != null &&
+                                  pickedDate != _endDate) {
+                                setState(() {
+                                  _endDate = pickedDate;
+                                });
+                              }
+                            },
+                            child: AbsorbPointer(
+                              child: TextFormField(
+                                controller: TextEditingController(
+                                    text: _endDate.toString().split(' ')[0]),
+                                decoration: const InputDecoration(
+                                  labelText: 'End Date',
+                                  icon: Icon(Icons.calendar_today),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            )),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  createOrUpdateAnime();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: !isUpdate
-                      ? Colors.purple
-                      : Colors.blue, // Set background color
-                  foregroundColor: Colors.white,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        createOrUpdateAnime();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: !isUpdate
+                            ? Colors.purple
+                            : Colors.blue, // Set background color
+                        foregroundColor: Colors.white,
+                      ),
+                      child: !isUpdate
+                          ? const Text('Save')
+                          : const Text('Update')),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        cancelTextEditing();
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.orange, // Set background color
+                        foregroundColor: Colors.white,
+                      ),
+                      child: const Text('Cancel')),
+                ],
+              ),
+              const Divider(),
+              Expanded(
+                child: FutureBuilder(
+                  future: anime,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(child: Text('No Data'));
+                    } else {
+                      return generateList(snapshot.data!);
+                    }
+                  },
                 ),
-                child: !isUpdate ? const Text('Save') : const Text('Update')),
-            const SizedBox(
-              width: 10,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  cancelTextEditing();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.orange, // Set background color
-                  foregroundColor: Colors.white,
-                ),
-                child: const Text('Cancel')),
-          ],
+              ),
+            ],
+          ),
         ),
-        const Divider(),
-        Expanded(
-            child: FutureBuilder(
-          future: anime,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Text('No Data');
-            }
-            if (snapshot.hasData) {
-              return generateList(snapshot.data!);
-            }
-            return const CircularProgressIndicator();
-          },
-        ))
-      ],
-    ));
+      ),
+    );
   }
 
   Widget generateList(List<Anime> anime) {
@@ -193,8 +318,24 @@ class _AnimeScreenState extends State<AnimeScreen> {
             ),
           ],
         ), // Assuming each anime has a unique id
-        child: ListTile(title: Text(anime[index].title)),
+        child: ListTile(
+          title: Text(anime[index].title),
+          subtitle: Text(
+            'Status: ${anime[index].status}, Progress: ${anime[index].progress}, Score: ${anime[index].score ?? 'N/A'}, Start Date: ${_formatDateTime(anime[index].startDate) ?? 'N/A'}, End Date: ${_formatDateTime(anime[index].endDate) ?? 'N/A'}',
+          ),
+        ),
       ),
     );
+  }
+
+  String? _formatDateTime(DateTime? dateTime) {
+    return dateTime != null
+        ? '${dateTime.year}-${_twoDigits(dateTime.month)}-${_twoDigits(dateTime.hour)}:${_twoDigits(dateTime.minute)}'
+        : null;
+  }
+
+  String _twoDigits(int n) {
+    if (n >= 10) return '$n';
+    return '0$n';
   }
 }
--- lib/dto/anime.dart
+++ lib/dto/anime.dart
@@ -1,12 +1,24 @@
 class Anime {
   late int? id;
   late String title;
-  Anime(this.id, this.title);
+  late String status;
+  late int progress;
+  late double? score;
+  late DateTime? startDate;
+  late DateTime? endDate;
+
+  Anime(this.id, this.title, this.status, this.progress, this.score,
+      this.startDate, this.endDate);
 
   Map<String, dynamic> toMap() {
     var map = <String, dynamic>{
       'id': id,
       'title': title,
+      'status': status,
+      'progress': progress,
+      'score': score,
+      'start_date': startDate,
+      'end_date': endDate,
     };
     return map;
   }
@@ -14,5 +26,10 @@ class Anime {
   Anime.fromMap(Map<String, dynamic> map) {
     id = map['id'];
     title = map['title'];
+    status = map['status'];
+    progress = map['progress'];
+    score = map['score'];
+    startDate = map['start_date'];
+    endDate = map['end_date'];
   }
 }
